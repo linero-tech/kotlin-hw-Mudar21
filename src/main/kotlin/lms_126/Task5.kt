65af23d0cbde08336ac5e4bcd6d3b5f892a0fdd6
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-09-02 01:08:59 +0200

    task 5
    ps: i wanted to write this whole thing without the "ELSE" and then after i wrote an "if" for every day of the week ,i wanted to add at the end of it "ELSE"
    "OUTPUT" ("error" or "something went wrong") u know so if X=8  it  will type that instead of a day of the week.

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -1,6 +1,20 @@
 package lms_126
 
 fun task5() {
-
-    TODO("Erase this line and write your solution")
+/* INPUT "x"
+IF "x" =1
+OUTPUT "monday"
+ELSE IF "x" =2
+OUTPUT "tuesday"
+ELSE IF "x" =3
+OUTPUT "wednesday"
+ELSE IF "x" =4
+OUTPUT "thursday"
+ELSE IF "x" =5
+OUTPUT "friday"
+ELSE IF "x" =6
+OUTPUT "saturday"
+ELSE IF "x" =7
+OUTPUT "sunday"
+ */
 }

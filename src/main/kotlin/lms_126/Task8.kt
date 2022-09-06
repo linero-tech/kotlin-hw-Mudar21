19287b57b94c27b77fd54fd2299d2ee90366e1a1
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-09-06 04:55:40 +0200

    task 8

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -1,6 +1,13 @@
 package lms_126
 
 fun task8() {
-
-    TODO("Erase this line and write your solution")
+    /*INPUT x
+    IF x%3=0
+    OUTPUT Foo
+    ELSE IF x%5=0
+    OUTPUT Bar
+    ELSE IF x%3=0 % x%5=0
+    OUTPUT FooBar
+    ELSE OUTPUT Qix
+     */
 }

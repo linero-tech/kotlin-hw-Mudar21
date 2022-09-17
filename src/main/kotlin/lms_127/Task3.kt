c15231b07e674a742ead37cf7ee0984d48adf445
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-09-17 19:21:18 +0200

    task3

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -1,7 +1,5 @@
 package lms_127
 
-import lms_126.task3
-
 
 fun task3(radius: Double): Double {
     val result =2.0 * 3.1416 * radius
@@ -9,5 +7,5 @@ fun task3(radius: Double): Double {
     return result
 }
 fun main () {
-    task3()
+    task3(radius = 2.0)
 }

78062316f9f636f96624e7b054089ccb26dab50d
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-09-24 17:03:57 +0200

    task1

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,9 +1,9 @@
 package lms_127
 
 fun task1() {
-    val country= "Sweden"
-    val capital= "Stockholm"
-    val currency= "SEK"
+    val country="Sweden"
+    val capital="Stockholm"
+    val currency="SEK"
     println("Country: $country")
     println("Capital: $capital")
     println("Currency: $currency")

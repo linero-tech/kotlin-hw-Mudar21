197d70be57620e721b5a9d285feb3477a1555976
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-12 21:56:58 +0200

    task2

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -4,9 +4,8 @@ fun task2(number: Int): Boolean {
 var result= false
     if (number % 2 == 1) result=true
     else if (number == 2) result=true
-    println(result)
     return result
 }
 fun main(){
-    task2(number = 25)
+  println(task2(number = 5))
 }

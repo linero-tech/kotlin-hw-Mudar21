e1d3dee9787998adeea5801babe9b1d88c1d28d9
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-12 21:46:27 +0200

    task 2

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -1,6 +1,21 @@
 package lms_130
 
 fun task2(items: List<Int>): Int {
+    var result=0
+    for(number in items.indices){
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+        var index= items[number]
+
+        if(number % 2==0)
+
+            result += items[number]
+        else if(items.isEmpty()){
+            result = 0
+        }
+
+    }
+    return result
+}
+fun main(){
+    println(task2(items = listOf()))
 }

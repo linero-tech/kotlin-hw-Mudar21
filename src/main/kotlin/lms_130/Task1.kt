840a519d682f133fcc5849ca7fa8de288f6c77dc
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-12 16:59:29 +0200

    task1

--- src/main/kotlin/lms_130/Task1.kt
+++ src/main/kotlin/lms_130/Task1.kt
@@ -1,6 +1,15 @@
 package lms_130
 
 fun task1(items: List<Int>): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+var result=0
+    if (items.isNotEmpty()){
+        result+=items.random()
+    }
+    else{
+        result=0
+    }
+    return result
+}
+fun main(){
+    println(task1(items = listOf()))
 }

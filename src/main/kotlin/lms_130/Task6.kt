9a2d6ec1905c614a1877b317d8082bc0bd09a852
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-16 18:05:07 +0200

    task6

--- src/main/kotlin/lms_130/Task6.kt
+++ src/main/kotlin/lms_130/Task6.kt
@@ -1,6 +1,10 @@
 package lms_130
 
 fun task6(sentence: String): String {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = sentence.uppercase()
+    println(result)
+    return result
+}
+fun main(){
+    task6(sentence = "I like Gothenburg")
 }

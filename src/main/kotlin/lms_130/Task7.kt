632839a55af4f2c76042ccc049c1ff07b5a56aff
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-19 20:29:31 +0200

    task7

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -1,15 +1,11 @@
 package lms_130
 
 fun task7(items: List<Int>): Int {
-    var result = 0
-    result = if (items.isEmpty()){
-        0
-    }else
-        items.first()
-    for (number in items){
-        if (number < result){
-            result = number
-        }
+ var result= if (items.isNotEmpty()) items.first() else 0
+    items.forEach {number ->
+if (number < result){
+result= number
+}
     }
     return result
 }

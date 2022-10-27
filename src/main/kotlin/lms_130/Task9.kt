e3e21189fbe4bfaa692194c0ce4df14bed56715f
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-27 17:32:04 +0200

    task9//104

--- src/main/kotlin/lms_130/Task9.kt
+++ src/main/kotlin/lms_130/Task9.kt
@@ -1,6 +1,17 @@
 package lms_130
 
 fun task9(items: List<Int>): List<Int> {
+    val result = mutableListOf<Int>()
+    items.forEachIndexed { index, number ->
+        if (index == 0) {
+            result.add(number * number)
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+        } else {
+            result.add(number * items[index - 1])
+        }
+    }
+    return result
+}
+fun main() {
+    println(task9(items = listOf(5,2,3,4)))
 }

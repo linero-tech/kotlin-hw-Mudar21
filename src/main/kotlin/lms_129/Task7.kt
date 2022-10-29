69518d51af00ff34b15c7cdea0a8387b39a4ac4d
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 22:20:15 +0200

    task7//103

--- src/main/kotlin/lms_129/Task7.kt
+++ src/main/kotlin/lms_129/Task7.kt
@@ -1,6 +1,12 @@
 package lms_129
 
 fun task7(a: Int, b: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 1
+    for(count in 1..b){
+        result *= a
+    }
+    return result
+}
+fun main(){
+    println(task7(a=2, b=3))
 }

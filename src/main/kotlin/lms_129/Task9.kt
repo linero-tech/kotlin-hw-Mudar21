9301bfd5c61f023ee90e9b686113316b1234cb02
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 19:01:25 +0200

    task9//103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task9(temperature: String): String {
+    val magnitude = temperature.substring(0, temperature.length - 1).toDouble()
+    val isC = temperature.contains("c", ignoreCase = true)
+    val result = if (isC) {
+        val conversion = (9.0 * magnitude / 5.0) + 32.0
+        "${conversion.toInt()}F"
+    } else {
+        val conversion = (5.0 * (magnitude - 32.0)) / 9.0
+        "${conversion.toInt()}C"
+    }
+    return result
+}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun main() {
+    println(task9(temperature = "50f"))
 }

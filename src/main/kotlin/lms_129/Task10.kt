ceb342b57acbbfca0982e210b686f7a5468dad67
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 23:05:23 +0200

    task10//103yet another attempt

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -1,16 +1,16 @@
 package lms_129
 
 fun task10(password: String): Boolean {
-    val result = true
-    if (password.length < 6) return false
-    else if (password.length > 10) return false
-    if (password.firstOrNull { it.isDigit() } == null) return false
-    if (password.filter { it.isLetter() }.firstOrNull { it.isUpperCase() } == null) return false
-    if (password.filter { it.isLetter() }.firstOrNull { it.isLowerCase() } == null) return false
-    if (password.firstOrNull { !it.isLetterOrDigit() } == null) return false
-    return result
-}
+        val result = true
+        if (password.length < 6) return false
+        else if (password.length > 10) return false
+        if (password.firstOrNull { it.isDigit() } == null) return false
+        if (password.filter { it.isLetter() }.firstOrNull { it.isUpperCase() } == null) return false
+        if (password.filter { it.isLetter() }.firstOrNull { it.isLowerCase() } == null) return false
+        if (password.firstOrNull { !it.isLetterOrDigit() } == null) return false
+        return result
+    }
 
-fun main() {
-    println(task10("123456dD$7"))
-}
+    fun main(){
+        println(task10("123456dD$7"))
+    }

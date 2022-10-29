02bcf7e09ff4e9cfbc428e410a887b038f142386
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 22:33:12 +0200

    task12//104 push 2

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -1,25 +1,26 @@
 package lms_130
 
+import lms_131.task12
+
 fun task12_1(sells: List<Int>): String {
 
     val days = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     val maxSellValue = sells.maxOrNull()
     val indexOfMaxSellValue = sells.indexOf(maxSellValue)
     val result = days[indexOfMaxSellValue]
-
-    println(result)
-
     return result
 }
 
 fun task12_2(sells: List<Int>): String {
-
+    
     val days = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     val maxSetlValue = sells.minOrNull()
     val indexOfMaxSellValue = sells.indexOf(maxSetlValue)
     val result = days[indexOfMaxSellValue]
-
-    println(result)
-
     return result
+}
+
+fun main() {
+    println(task12_1(sells = listOf(20, 100, 800, 768, 765, 90, 10)))
+    println(task12_2(sells= listOf(20, 100, 800, 768, 765, 90, 10)))
 }

9123eeb90468184583299ad4f4b3b2e3e7c052f3
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 22:26:33 +0200

    task12//104

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -2,15 +2,24 @@ package lms_130
 
 fun task12_1(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val days = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val maxSellValue = sells.maxOrNull()
+    val indexOfMaxSellValue = sells.indexOf(maxSellValue)
+    val result = days[indexOfMaxSellValue]
+
+    println(result)
+
+    return result
 }
 
 fun task12_2(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+    val days = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val maxSetlValue = sells.minOrNull()
+    val indexOfMaxSellValue = sells.indexOf(maxSetlValue)
+    val result = days[indexOfMaxSellValue]
 
-fun task12_3(sells: List<Int>): Int {
+    println(result)
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
 }

31c126c6781bbb71a212580f2f85068d297bb057
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-10-29 22:38:28 +0200

    task 12 //104 ,

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -8,19 +8,32 @@ fun task12_1(sells: List<Int>): String {
     val maxSellValue = sells.maxOrNull()
     val indexOfMaxSellValue = sells.indexOf(maxSellValue)
     val result = days[indexOfMaxSellValue]
+
+    println(result)
+
     return result
 }
 
 fun task12_2(sells: List<Int>): String {
-    
+
     val days = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     val maxSetlValue = sells.minOrNull()
     val indexOfMaxSellValue = sells.indexOf(maxSetlValue)
     val result = days[indexOfMaxSellValue]
+
+    println(result)
+
     return result
 }
 
-fun main() {
-    println(task12_1(sells = listOf(20, 100, 800, 768, 765, 90, 10)))
-    println(task12_2(sells= listOf(20, 100, 800, 768, 765, 90, 10)))
+fun task12_3(sells: List<Int>): Int {
+
+    var result = 0
+    sells.forEach {
+        result += it
+    }
+
+    println(result)
+
+    return result
 }

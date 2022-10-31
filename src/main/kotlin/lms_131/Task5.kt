2ab4597654e4d3520d1e23e2612bba2465babcd1
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-11-01 00:16:19 +0100

    task5//105

--- src/main/kotlin/lms_131/Task5.kt
+++ src/main/kotlin/lms_131/Task5.kt
@@ -1,6 +1,15 @@
 package lms_131
 
 fun task5(mapa: Map<Int, Int>, constant: Int): List<Int> {
+    val result = mutableListOf<Int>()
+    mapa.forEach { (key, value) ->
+        if (value !== constant) {
+            result += key
+        }
+    }
+    return result
+}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun main() {
+    println(task5(mapa = mapOf(1 to 100, 3 to 200, 4 to 500), constant = 200))
 }

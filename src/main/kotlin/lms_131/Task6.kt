0c825d0c2ffb3fe12eeeaf84431843e535160ac3
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-11-01 00:47:12 +0100

    task6//105

--- src/main/kotlin/lms_131/Task6.kt
+++ src/main/kotlin/lms_131/Task6.kt
@@ -1,6 +1,11 @@
 package lms_131
 
 fun task6(mapa: Map<Int, String>, parameter: String): Map<Int, String> {
+    val result = mutableMapOf<Int, String>()
+    result += mapa.filterValues { it == parameter }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+fun main() {
+    println(task6(mapa = mapOf(20 to "Ana", 40 to "John", 22 to "Ana"), parameter = "Ana"))
 }

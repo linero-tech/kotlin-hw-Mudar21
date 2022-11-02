b8248a6cb362b51cddbee911f1707ddec70ae2b8
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-11-02 23:43:59 +0100

    task11//105

--- src/main/kotlin/lms_131/Task11.kt
+++ src/main/kotlin/lms_131/Task11.kt
@@ -1,6 +1,15 @@
 package lms_131
 
 fun task11(mapa: Map<Int, String>, prospect: Int): Boolean {
+    var result = false
+    mapa.forEach { entry ->
+        if (entry.key == prospect) {
+            result = true
+        }
+    }
+    return result
+}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun main() {
+    println(task11(mapa = mapOf(1 to "A", 2 to "B", 3 to "C"), prospect = 3))
 }

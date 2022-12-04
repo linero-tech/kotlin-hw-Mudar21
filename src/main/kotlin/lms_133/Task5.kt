b3fee46c55762ad38397361cecea9ac6ab46e96d
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-12-04 23:32:30 +0100

    task5 /107

--- src/main/kotlin/lms_133/Task5.kt
+++ src/main/kotlin/lms_133/Task5.kt
@@ -1,3 +1,21 @@
 package lms_133
 
-// TODO(Create your function here)
+fun groceries(list: List<Map<String, Any>>): Double {
+    var result = 0.0
+    list.forEach { map ->
+        result += map.getValue("quantity").toString().toInt() * map.getValue("price").toString().toDouble()
+    }
+
+    return result
+}
+
+fun main() {
+    println(
+        groceries(
+            list = listOf(
+                mapOf("product" to "Milk", "quantity" to 3, "price" to 1.50),
+                mapOf("product" to "Meat", "quantity" to 3, "price" to 2.50)
+            )
+        )
+    )
+}

2c4a0adefa68ca8f7c3d8a0fd36362524ce485fe
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-12-05 00:18:51 +0100

    task4//107

--- src/main/kotlin/lms_133/Task4.kt
+++ src/main/kotlin/lms_133/Task4.kt
@@ -2,5 +2,5 @@ package lms_133
 
 fun washing(washes: Int, months: Int): Double = (months * 30 * washes * 21).toDouble() / 60
 fun main() {
-    println(washing(washes = 678, months = 768))
+    println(washing(washes = 8, months = 7))
 }

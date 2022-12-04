0db23a352b3a6f7a36e9cc55ca1bb9f2fe47cae5
Mudar21 <112378156+Mudar21@users.noreply.github.com>
2022-12-05 00:19:37 +0100

    task6//107

--- src/main/kotlin/lms_133/Task6.kt
+++ src/main/kotlin/lms_133/Task6.kt
@@ -1,3 +1,13 @@
 package lms_133
 
-// TODO(Create your function here)
+
+fun counter(word: String): Boolean {
+    val alphabets = ('a'..'z').toList()
+    var sum = 0
+    for (letter in word.lowercase()) if (letter in alphabets) sum += alphabets.indexOf(letter) + 1
+    return (sum % 2 == 0)
+}
+
+fun main() {
+    println(counter("ba"))
+}
